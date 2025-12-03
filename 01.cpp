18ee7bdf386320d01a29e0d8cf3e731dc1717f70
adhirajChauhan <adhirajsingh0105@gmail.com>
2025-12-03 20:39:17 +0530

    My first commit

---
+++ 01.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+// int -> 4 byte -> 5
+// float -> 4 byte -> 5.7
+//double -> 8 byte -> 5.77777
+// char -> 1 byte -> 'a'
+// bool -> 1 byte -> true/false
+
+
+
+
+
+int main()
+{
+    int num;
+    cout << "Enter a number : " << endl;
+    cin >> num;
+
+    cout << "Number is : " + num;
+}
